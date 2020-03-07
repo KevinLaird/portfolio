fe9c6dda7e633bf990bd7b7d71b28dcfa79602cd
Kevin Laird <kevinlaird@Kevins-MacBook-Pro.local>
2020-03-07 14:04:45 -0800

    commit on 'master'

--- src/react/components/references/reference/Reference.js
+++ src/react/components/references/reference/Reference.js
@@ -2,23 +2,23 @@ import React, { Component } from 'react'
 
 export class Reference extends Component {
     render() {
-        return (
+        return this.props.references.map((reference) => (
                 <div className="reference__item">
                     <div className="reference__item--profile">
-                        <img className="reference__item--profile-img" src="./assets/reference/Abe.jpeg" alt="Abraham Avnisan"></img>
+                        <img className="reference__item--profile-img" src={ reference.image } alt={ reference.title }></img>
                         <div className="reference__item--title-box">
-                            <h2 className="h2 reference__item--title">Abraham Avnisan</h2>
-                            <p className="body-1 reference__item--subtitle">University of Washington Professor - Supervisor</p>
+                            <h2 className="h2 reference__item--title">{ reference.title }</h2>
+                            <p className="body-1 reference__item--subtitle">{ reference.subtitle }</p>
                         </div>
                     </div> 
                     <p className="body-1 style-2">
-                        I had the pleasure of working with Kevin across his two years at University of Washington Bothell's Interactive Media Design program, including during his last semester when he worked as the UI/UX Designer for his team's capstone project.
+                        {reference.testimony1}
                     </p> 
                     <p className="body-1 style-2">
-                        Kevin is a talented designer with an exceptional work ethic. He is passionate about what he does, and it comes through not only in the high quality of work that he produces, but in the positive relationships he forges with the people he collaborates with. As a team player, Kevin is a good listener and compromiser, but he also isn't afraid to advocate for a position he strongly believes in. I am confident that Kevin will make a positive and valuable addition to any company or project he contributes to. 
+                        {reference.testimony2}                    
                     </p>
                 </div>
-        )
+        ));
     }
 }
 
